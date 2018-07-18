38f3d239a0201cf3903f2e3f0b52d70120151e87
Carlos Mañas <carlos@spacenomads.com>
2018-07-18 18:05:32 +0200

    Add basic data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 'use strict';
 
+const data = [
+  {
+    title: 'Asteroids 101',
+    image: ''
+  },
+  {
+    title: 'Life on Mars',
+    image: ''
+  },
+  {
+    title: 'Martians invade Earth',
+    image: ''
+  },
+  {
+    title: 'Humans aren't real',
+    image: ''
+  },
+  {
+    title: 'Space The Final Frontier',
+    image: ''
+  }
+];
 
 
