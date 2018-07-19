d6aaf467021e0674612e70ba2d26d35e67c4fa92
SandraCGarcia <sandra.gcanete@gmail.com>
2018-07-19 22:26:45 +0200

    ejercicio hecho

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const element = document.querySelector('.news');
+
 const data = [
   {
     title: 'Asteroids 101',
@@ -22,3 +24,10 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+for (let i = 0; i < data.length; i++) {
+  element.innerHTML += `<li class="news_item"><h2 class="news__title">${data[i].title}</h2><img class="news__image" src="${data[i].image}" alt="${
+    data[i].title
+  }"> </li>`;
+}
+
