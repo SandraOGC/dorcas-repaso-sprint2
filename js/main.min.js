c4ca72ffb77871c9093ff235331f94d9a808f299
crstinapl <acpleiro@gmail.com>
2018-07-21 20:32:31 +0200

    No sé pq no me coge la variable...

--- js/main.min.js
+++ js/main.min.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var newElement = document.querySlector('.news');
+const newElement = document.querySlector('.news');
 // var list = document.querySlector('.news__item');
 // var title = document.querySlector('.news__title');
 // var image = document.querySlector('.news__image');
@@ -29,11 +29,13 @@ const data = [
 ];
 
 // function addElement(){
-
-for (var i = 0; i < data.length; i++){
-newElement.innerHTML += '<li class="news__item"><h2 class="news__title"> </h2><img  class="new__image" src=" " alt="">';
-
-  // }}
+// var newItem = document.createElement('li');
+// console.log(newItem);
+for (let i = 0; i < data.length; i++){
+  newElement.innerHTML += '<li class="news__item"><h2 class="news__title">${data[i].title} </h2><img  class="new__image" src="${data[i].image}" alt="${data[i].title}"></li>';
+  // newElement.innerHTML += '<li class="news__item"><h2 class="news__title"> </h2><img  class="new__image" src="https://via.placeholder.com/200x100" alt="(' + i + ')"></li>';
+}
+// }}
 // newElement.appendChild(list);
 // list.appendChild(title);
 // list.appendChild(image);
