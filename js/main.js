674ade349e5eb8259f3dc152b029be131d07663d
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-07-21 23:06:15 +0200

    do first exercise

--- js/main.js
+++ js/main.js
@@ -26,16 +26,19 @@ const data = [
 console.log(data[1]);
 const news = document.querySelector('.news');
 
-
+// painting tittle and image
 for(var i = 0; i < data.length; i++){
   var listElement = document.createElement('li');
   listElement.classList.add('.listElement');
   news.appendChild(listElement);
-  var filmTitle =document.createElement('p');
+  var filmTitle =document.createElement('h2');
   listElement.appendChild(filmTitle);
   var filmImage = document.createElement('img');
   listElement.appendChild(filmImage);
-
+  var titleOfTheFilm = data[i].title;
+  filmTitle.innerHTML = titleOfTheFilm;
+  var imageOfTheFilm = data[i].image;
+  filmImage.innerHTML = imageOfTheFilm;
+  filmImage.setAttribute('src', imageOfTheFilm);
+  filmImage.setAttribute('alt', titleOfTheFilm);
 }
-
-console.log(news);
