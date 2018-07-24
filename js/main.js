c473f595e3bafa4e441022e648b2c95590535a4f
SandraCGarcia <sandra.gcanete@gmail.com>
2018-07-24 15:24:38 +0200

    empiezo el ejercicio 4

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 'use strict';
 
-const data = [
-  {
-    title: "Asteroids 101",
-    image: "https://via.placeholder.com/200x100"
-  },
-  {
-    title: "Life on Mars",
-    image: "https://via.placeholder.com/200x100"
-  },
-  {
-    title: "Martians invade Earth",
-    image: "https://via.placeholder.com/200x100"
-  },
-  {
-    title: "Humans aren't real",
-    image: "https://via.placeholder.com/200x100"
-  },
-  {
-    title: "Space The Final Frontier",
-    image: "https://via.placeholder.com/200x100"
-  }
-];
+// const data = [
+//   {
+//     title: "Asteroids 101",
+//     image: "https://via.placeholder.com/200x100"
+//   },
+//   {
+//     title: "Life on Mars",
+//     image: "https://via.placeholder.com/200x100"
+//   },
+//   {
+//     title: "Martians invade Earth",
+//     image: "https://via.placeholder.com/200x100"
+//   },
+//   {
+//     title: "Humans aren't real",
+//     image: "https://via.placeholder.com/200x100"
+//   },
+//   {
+//     title: "Space The Final Frontier",
+//     image: "https://via.placeholder.com/200x100"
+//   }
+// ];
 
 
-const lista = document.querySelector('.news');
+// const lista = document.querySelector('.news');
 
-for (const bucle of data){
-  const list = document.createElement('li');
-  const titulo = document.createElement('h2');
-  const photo = document.createElement('img');
+// for (const bucle of data){
+//   const list = document.createElement('li');
+//   const titulo = document.createElement('h2');
+//   const photo = document.createElement('img');
 
-  lista.appendChild(list);
-  list.appendChild(titulo);
-  list.appendChild(photo);
+//   lista.appendChild(list);
+//   list.appendChild(titulo);
+//   list.appendChild(photo);
 
-  const text = document.createTextNode(bucle.title);
-  photo.src = bucle.image;
+//   const text = document.createTextNode(bucle.title);
+//   photo.src = bucle.image;
 
-  list.classList.add('news__item');
-  titulo.classList.add('news__title');
-  photo.classList.add('news__image');
-  titulo.appendChild(text);
-}
+//   list.classList.add('news__item');
+//   titulo.classList.add('news__title');
+//   photo.classList.add('news__image');
+//   titulo.appendChild(text);
+// }
 
 
 // const noticias = newsData => {
@@ -61,16 +61,34 @@ for (const bucle of data){
 
 // noticias(data);
 
-const newsMars = () => {
-  const newsItem = document.querySelectorAll ('.news__item');
+// const newsMars = () => {
+//   const newsItem = document.querySelectorAll ('.news__item');
 
-  for (const item of newsItem) {
-    const itemTitulo = item.querySelector('.news__title').innerHTML;
+//   for (const item of newsItem) {
+//     const itemTitulo = item.querySelector('.news__title').innerHTML;
 
-    if ( (itemTitulo.includes('Mars')) || (itemTitulo.includes('Martian')) ) {
-      item.classList.add('news__item--from-mars');
-    }
-  }
-};
+//     if ( (itemTitulo.includes('Mars')) || (itemTitulo.includes('Martian')) ) {
+//       item.classList.add('news__item--from-mars');
+//     }
+//   }
+// };
 
-newsMars ();
+// newsMars ();
+
+
+
+
+//ejercicio 3
+
+var list = document.querySelector(".news");
+var noticias = [];
+
+
+function show() {
+  fetch("http://api.tvmaze.com/search/shows?q=")
+    .then(function(respuesta) {
+      return respuesta.json();
+    })
+    .then(function(respuesta2) {
+      console.log(respuesta2);
+      noticias = respuesta2;
