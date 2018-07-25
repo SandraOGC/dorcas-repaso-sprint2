554b52cb3ea02b5672d898e3cf1cd5a2efc8a602
crstinapl <acpleiro@gmail.com>
2018-07-25 16:10:44 +0200

    prove exercise 3

--- js/main.js
+++ js/main.js
@@ -1,59 +1,90 @@
 'use strict';
 
 const newsElement = document.querySelector('.news');
+const news__item = document.createElement ('ĺi');
+const news__title = document.createElement ('h2');
+const news__image = document.createElement('img');
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
+news__item.classList.add('news__item'); news__title.classList.add('news__title');
+news__image.classList.add('news__image');
 
-const addList = () => {
-  for(let planet of data) {
-    //crear elementos
-    //tengo cambiar los nombres por los que me han dado
-    const news__item = document.createElement ('ĺi');
-    const news__title = document.createElement ('h2');
-    const news__image = document.createElement('img');
-    //poner clases
-    news__item.classList.add('news__item');
-    news__title.classList.add('news__title');
-    news__image.classList.add('news__image');
-    //hijos
-    newsElement.appendChild(news__item);
-    news__item.appendChild(news__title);
-    news__item.appendChild(news__image);
-    //pintar elementos
-    //ahora mi [i] del bucle se llama planet
-    news__title.innerHTML = planet.title;
-    //src es la ruta de la imagen donde meto su URL
-    news__image.src = planet.image;
+newsElement.appendChild(news__item);
+news__item.appendChild(news__title);
+news__item.appendChild(news__image);
 
-    //EJERCICIO 2
-      console.log(news__title);
-    if (news__title.innerHTML.includes('Mars') || news__title.innerHTML.includes('Martians')){
-      news__item.classList.add('news__item--from-mars');
-    } else {
-      console.log('No contiene la palabra Mars ni Martian');
-    }
-  }
-};
-//por último, tengo que hacer la llamada de la función para que se ejecute
-addList();
+const button = document.createElement('boton');
+const input = document.createElement ('input');
+
+function showPlanet(){
+  fetch ('https://raw.githubusercontent.com/Adalab/dorcas-repaso-sprint2/master/data/news.json')
+    .then(function(response){
+      return response.json();
+    })
+    .then (function(json){
+      for(var i= 0; i<json.length; i++){
+        console.log('lol');
+        const title = document.createTextNode(json[i].news.title);
+        const image = document.createElement(json[i].news.image);
+      }
+    });
+}
+button.addEvenListener('click', showPlanet);
+
+// const newsElement = document.querySelector('.news');
+//
+// const data = [
+//   {
+//     title: 'Asteroids 101',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Life on Mars',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Martians invade Earth',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Humans aren\'t real',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Space The Final Frontier',
+//     image: 'https://via.placeholder.com/200x100'
+//   }
+// ];
+//
+// const addList = () => {
+//   for(const planet of data) {
+//     //crear elementos
+//     //tengo cambiar los nombres por los que me han dado
+//     const news__item = document.createElement ('ĺi');
+//     const news__title = document.createElement ('h2');
+//     const news__image = document.createElement('img');
+//     //poner clases
+//     news__item.classList.add('news__item');
+//     news__title.classList.add('news__title');
+//     news__image.classList.add('news__image');
+//     //hijos
+//     newsElement.appendChild(news__item);
+//     news__item.appendChild(news__title);
+//     news__item.appendChild(news__image);
+//     //pintar elementos
+//     //ahora mi [i] del bucle se llama planet
+//     news__title.innerHTML = planet.title;
+//     //src es la ruta de la imagen donde meto su URL
+//     news__image.src = planet.image;
+//
+//     //EJERCICIO 2
+//     console.log(news__title);
+//     //la condicional tb podría ser: if(planet.title.includes...)
+//     if (news__title.innerHTML.includes('Mars') || news__title.innerHTML.includes('Martians')){
+//       news__item.classList.add('news__item--from-mars');
+//     } else {
+//       console.log('No contiene la palabra Mars ni Martian');
+//     }
+//   }
+// };
+// //por último, tengo que hacer la llamada de la función para que se ejecute
+// addList();
