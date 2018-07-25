03bf182a49d2a6e0075ab9b312c6b03268e13a77
SandraCGarcia <sandra.gcanete@gmail.com>
2018-07-25 21:32:18 +0200

    borro ejercicio 4 para hacer otro

--- js/main.js
+++ js/main.js
@@ -1,129 +1,22 @@
 'use strict';
+const lista = document.querySelector('.nums');
 const texto = document.querySelector('.parrafo');
 
-// const data = [
-//   {
-//     title: 'Asteroids 101',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Life on Mars',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Martians invade Earth',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Humans aren't real',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Space The Final Frontier',
-//     image: 'https://via.placeholder.com/200x100'
-//   }
-// ];
+const numeros = [1, 4, 2, 5];
 
-// const lista = document.querySelector('.news');
+for (const list of numeros){
+  const listado = document.createElement('li');
+  const listadoP = document.createElement('p');
 
-// for (const bucle of data){
-//   const list = document.createElement('li');
-//   const titulo = document.createElement('h2');
-//   const photo = document.createElement('img');
+  const extraContent = document.createTextNode(list);
 
-//   lista.appendChild(list);
-//   list.appendChild(titulo);
-//   list.appendChild(photo);
+  listadoP.appendChild(extraContent);
+  listado.appendChild(listadoP);
+  listado.addEventListener('click', nose);
+  lista.appendChild(listado);
 
-//   const text = document.createTextNode(bucle.title);
-//   photo.src = bucle.image;
-
-//   list.classList.add('news__item');
-//   titulo.classList.add('news__title');
-//   photo.classList.add('news__image');
-//   titulo.appendChild(text);
-// }
-
-// const noticias = newsData => {
-//   const elementos = document.querySelector('.news');
-
-//   for (const item of newsData) {
-//     const elemento = `
-//       <li class='news__item'>
-//         <h2 class='news__title'>${item.title}</h2>
-//         <img class='news__image' src='${item.image}' alt='${item.title}'>
-//       </li>
-//     `;
-//     elementos.insertAdjacentHTML('beforeEnd', elemento);
-//   }
-// };
-
-// noticias(data);
-
-// const newsMars = () => {
-//   const newsItem = document.querySelectorAll ('.news__item');
-
-//   for (const item of newsItem) {
-//     const itemTitulo = item.querySelector('.news__title').innerHTML;
-
-//     if ( (itemTitulo.includes('Mars')) || (itemTitulo.includes('Martian')) ) {
-//       item.classList.add('news__item--from-mars');
-//     }
-//   }
-// };
-
-// newsMars ();
-
-//ejercicio 3
-
-var list = document.querySelector('.news');
-var noticias = [];
-
-const show = () => {
-  fetch(
-    'https://raw.githubusercontent.com/Adalab/dorcas-repaso-sprint2/master/data/news.json'
-  )
-    .then(function(respuesta) {
-      return respuesta.json();
-    })
-    .then(function(respuesta2) {
-      // console.log(respuesta2);
-      noticias = respuesta2.news;
-
-      for (const noticia of noticias) {
-        const lista = document.createElement('li');
-        const titulo = document.createElement('h2');
-        const photo = document.createElement('img');
-
-        lista.classList.add('news__item', 'news__item--no-image-visible');
-        titulo.classList.add('news__title');
-        photo.classList.add('news__image');
-
-        const text = document.createTextNode(noticia.title);
-        titulo.appendChild(text);
-        photo.src = noticia.image;
-
-        lista.append(titulo, photo);
-        lista.addEventListener('click', showImage);
-        list.appendChild(lista);
-      }
-
-        // visible();
-    });
-};
-
-show();
-
-// function visible() {
-//   var allLi = document.querySelectorAll('.news__item--no-image-visible');
-//   for (const oneLi of allLi) {
-//     oneLi.addEventListener('click', showImage);
-//   }
-// }
-
-function showImage(event) {
-  event.currentTarget.classList.toggle('news__item--no-image-visible');
-  texto.innerHTML=event.currentTarget.querySelector('.news__title').innerHTML;
 }
 
-//.innerhtml es para buscar en html el contenido (modificar y consultar)
+function nose (e){
+  texto.innerHTML = e.currentTarget.querySelector('p').innerHTML;
+}
